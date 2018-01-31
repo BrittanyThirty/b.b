41fe6ca1d444022b2ae48b5f744aca6fe8b23ca9
BrittanyThirty <bmkthirty@gmail.com>
2018-01-31 11:30:11 -0800

    adjust form structure in html, adjust css, restart approach to logic js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+//Back
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//UI
+
+
+
+
+
+
+
+
 function beepBoop(enterNum) {
 }
   for(var i = 0; i < enterNum; i++) {
@@ -8,7 +36,6 @@ function beepBoop(enterNum) {
     } else if (enterNum % 3===0) {
       console.log("I'm sorry Dave, 'fraid I can't do that!");
     } else {
-      alert("You don't need an alert here per last code review.")
     } return result;
   }
 });
@@ -16,7 +43,62 @@ var enterNum;
 var returnArr = [];
 
 
+var inputnum1 = [];
+function sorryDave() {
+  for (var i= 0; i < inputnum1.length; i++){
+  //if array is a multiple of 3
+  if(inputnum1[i] % 3 === 0){
+    //print out
+    console.log("Sorry Dave! I am afaid I cannot do that!");
+  }
+  }
+}
+
+
 $(document).ready(function() {
   $("form#words").submit(function(event)
     event.preventDefault();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//maybe a do while
+var i = 0; i < enterNum; i++ ;
+do {
+    console.log("Beep");
+    i++;
+} while(enterNum.includes(1))
+
+var j = 0; j < enterNum; j++ ;
+do {
+      console.log("Boop");
+    j++;
+} while(enterNum.includes(0))
+
+var k = 0; k < enterNum; i++ ;
+do {
+      console.log("I'm sorry Dave, 'fraid I can't do that!");
+    k++;
+} while(enterNum.includes % 3===0)
+
+//can the level of brevity...
+var i = 0;
+while(i < 5){
+    console.log(i);
+    i++;
 }
