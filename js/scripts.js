ffe8ed340df5c229791288981844507c8bfd2ae2
BrittanyThirty <bmkthirty@gmail.com>
2018-01-31 16:26:07 -0800

    adjust approach to solution in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
 //Back
-var yourNum;
-// var resultnumber = [];
 
-function beepBoop(yourNum){
-
+var bb = function(number) {
   for (var i= 0; i < result.length; i++){
-    if (enterNum.includes(1)) {
+var num = prompt("message");
+    if(num % 1 === 0 ) {
       console.log("Beep");
-    } else if (enterNum.includes(0)){
+    } else if (num % 1 === 1) {
       console.log("Boop");
-    } else if (resultnumber % 3 === 0){
-      console.log("I'm sorry Dave, 'fraid I can't do that!");
+    } else if (num % 3 === 0) {
+      console.log("Sorry Dave. I am araid I cannot do that!");
     } else {
-
+      return result;
     }
-    return result;
   }
-
 }
+
 
 //UI
 $(document).ready(function() {
@@ -26,45 +23,9 @@ $(document).ready(function() {
     event.preventDefault();
 
     var number = parseInt($("input#number").val());
-    var resultnumber = [];
+    var result = bb(number);
 
+    $(".number").text(number);
+     $("#result").show();
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-//
-// //maybe a do while
-// var i = 0; i < enterNum; i++ ;
-// do {
-//   console.log("Beep");
-//   i++;
-// } while(enterNum.includes(1))
-//
-// var j = 0; j < enterNum; j++ ;
-// do {
-//   console.log("Boop");
-//   j++;
-// } while(enterNum.includes(0))
-//
-// var k = 0; k < enterNum; i++ ;
-// do {
-//   console.log("Sorry Dave. I am araid I can't do that!");
-//   k++;
-// } while(enterNum.includes % 3===0)
-//
-// //can the level of brevity...
-// var i = 0;
-// while(i < 5){
-//   console.log(i);
-//   i++;
-// }
-//
