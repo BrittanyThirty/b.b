4af031345b6e68156b179715b5598467ddede1b1
BrittanyThirty <bmkthirty@gmail.com>
2018-02-01 02:24:00 -0800

    debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,30 @@
 //Back
-var number = function(userNumber) { //user inputs
-  var result = []; // initialize new array
+var numbers = function(userNumber) { //user inputs
+  var result = []; // initialize new
 
-//arr plus one arrive at appropriate location zero index
-  for(var index = 0; index <= userNumber; +=1) {
-  var stringNumber = index.toString();
+  for(var index = 0; index <= userNumber; i = i + 1) { //level up
+    var stringNumber = index.toString(); //per includes
 
-  if (index % 3 === 0 && index > 0) {
-    result.push("Sorry Dave. I'm afraid I can't do that!");
-  } else if (stringNumber.includes("0")) {
-  result.push("Beep!");
-} else if (stringNumber.includes("1")) {
-  result.push("Boop!");
-} else {
-  result.push(index);
-}
-return result;
-}
+    if (index % 3 === 0 && index > 0) { //divisible by three
+      result.push("Sorry Dave. I'm afraid I can't do that!"); //resultant text per assign
+    } else if (stringNumber.includes("0")) { //second condition check
+      result.push("Beep!"); //resultant text if foregoing met
+    } else if (stringNumber.includes("1")) { //third condition
+      result.push("Boop!"); //resultant
+    } else {
+      result.push(index); //if none of above cond met push index userNum
+    }
+
+  }
+    return result; //return state
+  }
 //UI
 $(document).ready(function() {
 
-  $("#formbb").submit(function(event) {
-    event.preventDefault();
-
-    var userNumber = parseInt($("#userInput").val());
-    var result = number(userNumber);
+  $("#formbb").submit(function(event) { //form upon submit
+    event.preventDefault(); // prevent user visibility issues
+    var userNumber = parseInt($("userInput").val()); //return a num-parse int
+    var result = numbers(userNumber);
 
     $("#result").show();
   });
