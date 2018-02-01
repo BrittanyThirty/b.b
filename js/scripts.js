84c19c9071f820079873c32259f01a2e38dc1004
BrittanyThirty <bmkthirty@gmail.com>
2018-02-01 02:01:30 -0800

    add comments, adjust logic (in js).

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,21 @@
 var number = function(userNumber) { //user inputs
   var result = []; // initialize new array
 
-  for(var index = 0; index <= userNumber; i++) {
+//arr plus one arrive at appropriate location zero index
+  for(var index = 0; index <= userNumber; +=1) {
   var stringNumber = index.toString();
 
   if (index % 3 === 0 && index > 0) {
     result.push("Sorry Dave. I'm afraid I can't do that!");
   } else if (stringNumber.includes("0")) {
-  result.push("beep!");
-} else if (airShove.includes(0)) {
-  $('.number').append(zero);
+  result.push("Beep!");
+} else if (stringNumber.includes("1")) {
+  result.push("Boop!");
 } else {
-
+  result.push(index);
+}
+return result;
+}
 //UI
 $(document).ready(function() {
 
@@ -20,7 +24,6 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userNumber = parseInt($("#userInput").val());
-    message(userNumber);
     var result = number(userNumber);
 
     $("#result").show();
